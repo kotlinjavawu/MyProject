b2ba5d9b3a910041eb54c07d7642082101d6b028
kotlinjavawu <wayne@typic-gp.com>
2021-02-18 15:39:04 +0800

    java and kotlin variable, data type

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -5,5 +5,27 @@ public class Hello {
 //        System.out.println("Hello World !");
         Person p = new Person();
         p.Hello();
+
+        //java 的8 種基本資料型態
+        int age = 19;
+        Integer age2 = 19;
+        age2.intValue();
+        short age3 = 20;
+        long age4 = 21;
+        char c = 'A';
+        Character c2 = 'B';
+        byte b = 120;
+        float weight = 66.5f;
+        Float weight2 = 70.8f;
+
+        double height = 1.7;
+        Double height2 = 2.5;
+
+        boolean adult = true;
+        boolean enroll = false;
+        Boolean adult2 = true;
+        //java的參 照資料型態 第一個字要大寫
+        String name = "Tom";
+
     }
 }
--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -4,6 +4,16 @@ fun main(args: Array<String>) {
     //println("Hello Kotlin !")
     val h = Human()
     h.Hello()
+
+    var age = 19;
+    age = 20
+    var weight : Float = 66.5f
+    val name = "Hank"
+    val name2:String
+    name2 = "Mary"
+
+
+
 }
 
 class Human{
