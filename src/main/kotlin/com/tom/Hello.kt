8a9c0d935e7f2775e30c9bca247aa986098915ed
kotlinjavawu <wayne@typic-gp.com>
2021-02-18 14:05:25 +0800

    Initial commit

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,13 @@
+package com.tom
+
+fun main(args: Array<String>) {
+    //println("Hello Kotlin !")
+    val h = Human()
+    h.Hello()
+}
+
+class Human{
+    fun Hello(){
+        println("Hello Kotlin !")
+    }
+}
